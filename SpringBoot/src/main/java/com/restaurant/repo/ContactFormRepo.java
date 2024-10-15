49f33d0899c9d29989696257c9f896c67f102804
aniket483 <102342478+aniket483@users.noreply.github.com>
2024-10-15 06:46:22 +0530

    Delete SpringBoot directory

--- SpringBoot/src/main/java/com/restaurant/repo/ContactFormRepo.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.restaurant.pojo.ContactForm;
-
-public interface ContactFormRepo extends JpaRepository<ContactForm, Long> {
-
-}
